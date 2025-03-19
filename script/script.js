9dd8f543a34d42268cf49a71358960ae9fb8ddeb
suchitsheth <suchitsheth42@gmail.com>
2025-03-19 23:59:22 +0530

    feat: add download button and twiiter share button working

--- script/script.js
+++ script/script.js
@@ -5,6 +5,24 @@ const quoteElement = blockQuoteElement.querySelector("p");
 const authorElement = blockQuoteElement.querySelector("footer cite");
 const copyButtonElement = mainElement.querySelector("#copy");
 const refetchButtonElement = mainElement.querySelector("#refetch");
+const downloadButtonElement = mainElement.querySelector("#download");
+const tweetButtonElement = mainElement.querySelector(".twitter-share-button");
+const actionElementsWrapper =
+  blockQuoteElement.querySelector(".action-wrapper");
+
+function generateQuoteWithAuthorInString(quote, author) {
+  return `"${quote}" -${author}`;
+}
+function generateTweetUrl(stringContent = "") {
+  if (!stringContent) {
+    return "";
+  }
+  const base = new URL("https://twitter.com/intent/tweet");
+  const params = new URLSearchParams({
+    text: encodeURI(stringContent),
+  });
+  return `${base.toString()}?${params.toString()}`;
+}
 
 function toggleLoading(status = false) {
   const isLoadingActive = loadingElement.classList.contains("active");
@@ -12,11 +30,16 @@ function toggleLoading(status = false) {
     loadingElement.classList.remove("active");
     copyButtonElement.disabled = false;
     refetchButtonElement.disabled = false;
+    downloadButtonElement.disabled = false;
+    tweetButtonElement.classList.remove("isDisabled");
+
     return;
   }
   loadingElement.classList.add("active");
   copyButtonElement.disabled = true;
   refetchButtonElement.disabled = true;
+  downloadButtonElement.disabled = true;
+  tweetButtonElement.classList.add("isDisabled");
 }
 
 async function fetchRandomQuoteFromApi() {
@@ -34,8 +57,13 @@ async function fetchRandomQuoteFromApi() {
 
     quoteElement.innerText = content;
     authorElement.innerText = author;
+    tweetButtonElement.setAttribute(
+      "href",
+      generateTweetUrl(generateQuoteWithAuthorInString(content, author))
+    );
   } catch (error) {
-    console.error(error);
+    console.log("Failed to get data from api:", error);
+    alert("Could not able to fetch new quote from api!!!");
   } finally {
     toggleLoading(false);
   }
@@ -43,7 +71,10 @@ async function fetchRandomQuoteFromApi() {
 
 function copyClickHandler() {
   navigator.clipboard.writeText(
-    `"${quoteElement.innerText}" -${authorElement.innerText}`
+    generateQuoteWithAuthorInString(
+      quoteElement.innerText,
+      authorElement.innerText
+    )
   );
   alert("Copy quote into your clipboard");
   document.activeElement?.blur?.();
@@ -54,6 +85,30 @@ function refetchClickHandler() {
   document.activeElement?.blur?.();
 }
 
+async function downloadClickHandler() {
+  if (!domtoimage) {
+    alert("Something went wrote while downloading as png!!!");
+  }
+  actionElementsWrapper.style.opacity = 0;
+  try {
+    const dataUrl = await domtoimage.toJpeg(mainElement, { quality: 0.95 });
+    var link = document.createElement("a");
+    const content = [quoteElement.innerText, authorElement.innerText]
+      .join(" by ")
+      .split(" ")
+      .map((item) => item.toLocaleLowerCase())
+      .join("-");
+    link.download = `${content}.jpeg`;
+    link.href = dataUrl;
+    link.click();
+  } catch (error) {
+    console.log("Download Issue Found Error:", error);
+    alert("Something went wrote while downloading as png!!!");
+  } finally {
+    actionElementsWrapper.style.opacity = 1;
+  }
+}
+
 document.addEventListener("DOMContentLoaded", () => {
   fetchRandomQuoteFromApi();
 });
@@ -61,3 +116,5 @@ document.addEventListener("DOMContentLoaded", () => {
 copyButtonElement.addEventListener("click", copyClickHandler);
 
 refetchButtonElement.addEventListener("click", refetchClickHandler);
+
+downloadButtonElement.addEventListener("click", downloadClickHandler);
